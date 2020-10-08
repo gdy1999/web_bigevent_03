1bb404b2ac8101d12de0145aba702dd7150a344f
94026090@qq.com <guodanyang@qq.com>
2020-10-08 09:08:22 +0800

    完成了登录和注册的功能

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,74 +1,83 @@
-$(function() {
-  // 点击“去注册账号”的链接
-  $('#link_reg').on('click', function() {
-    $('.login-box').hide()
-    $('.reg-box').show()
-  })
+$(function () {
+    // 给登录区域绑定事件
+    $("#link_reg").on("click", function () {
+        $(".login-box").hide();
+        $(".reg-box").show();
+    });
 
-  // 点击“去登录”的链接
-  $('#link_login').on('click', function() {
-    $('.login-box').show()
-    $('.reg-box').hide()
-  })
+    // 给注册区域绑定事件
+    $("#link_login").on("click", function () {
+        $(".login-box").show();
+        $(".reg-box").hide();
+    });
 
-  // 从 layui 中获取 form 对象
-  var form = layui.form
-  var layer = layui.layer
-  // 通过 form.verify() 函数自定义校验规则
-  form.verify({
-    // 自定义了一个叫做 pwd 校验规则
-    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-    // 校验两次密码是否一致的规则
-    repwd: function(value) {
-      // 通过形参拿到的是确认密码框中的内容
-      // 还需要拿到密码框中的内容
-      // 然后进行一次等于的判断
-      // 如果判断失败,则return一个提示消息即可
-      var pwd = $('.reg-box [name=password]').val()
-      if (pwd !== value) {
-        return '两次密码不一致！'
-      }
-    }
-  })
+    // 校验
+    // 从layui中获取到form
+    var form = layui.form;
+    form.verify({
+        psw: [
+            /^[\S]{6,12}$/
+            , '密码必须6到12位，且不能出现空格'
+        ],
+        // 确认密码
+        repsw: function (value) {
+            // 形参里面的参数是 确认密码里面的值
 
-  // 监听注册表单的提交事件
-  $('#form_reg').on('submit', function(e) {
-    // 1. 阻止默认的提交行为
-    e.preventDefault()
-    // 2. 发起Ajax的POST请求
-    var data = {
-      username: $('#form_reg [name=username]').val(),
-      password: $('#form_reg [name=password]').val()
-    }
-    $.post('/api/reguser', data, function(res) {
-      if (res.status !== 0) {
-        return layer.msg(res.message)
-      }
-      layer.msg('注册成功，请登录！')
-      // 模拟人的点击行为
-      $('#link_login').click()
-    })
-  })
-
-  // 监听登录表单的提交事件
-  $('#form_login').submit(function(e) {
-    // 阻止默认提交行为
-    e.preventDefault()
-    $.ajax({
-      url: '/api/login',
-      method: 'POST',
-      // 快速获取表单中的数据
-      data: $(this).serialize(),
-      success: function(res) {
-        if (res.status !== 0) {
-          return layer.msg('登录失败！')
+            // 获取输入框的密码
+            var psw = $(".reg-box [name = password]").val();
+            if (psw !== value) {
+                return "两次密码不一致,请重新输入";
+            }
         }
-        layer.msg('登录成功！')
-        // 将登录成功得到的 token 字符串，保存到 localStorage 中
-        localStorage.setItem('token', res.token)
-        // 跳转到后台主页
-        location.href = '/index.html'
-      }
     })
-  })
+
+    // 获取layer
+    var layer = layui.layer;
+    // 给注册区域绑定监听事件
+    $("#form_reg").on("submit", function (e) {
+        // 阻止表单默认提交事件
+        e.preventDefault();
+        // 获取用户名的值
+        var username = $("#form_reg [name=username]").val();
+        // 获取密码框的值
+        var password = $("#form_reg [name=password]").val();
+        // 发送ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/api/reguser',
+            data: {username: username,password: password},
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg("小伙子,恭喜你注册成功了");
+                // 模仿人的点击登录按钮
+                $("#link_login").click();
+            }
+        })
+    })
+
+
+    // 给登录区域绑定监听事件
+    $("#form_login").on("submit", function (e) {
+        // 阻止表单的默认提交
+        e.preventDefault();
+        // 发送ajax请求
+        $.ajax({
+            method: 'POST',
+            url: '/api/login',
+            // 快速获取表单数据
+            data: $(this).serialize(),
+            success: function (res) {
+                if (res.status !== 0) {
+                    return layer.msg(res.message);
+                }
+                layer.msg("登录成功");
+                // 保存token值 后面需要使用
+                localStorage.setItem("token", res.token);
+                // 跳转到后台主页面
+                location.href = "/index.html";
+            }
+        })
+    })
 })
